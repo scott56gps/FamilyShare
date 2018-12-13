68cc79bb5527ed38beaa14a935dcc8ae568ad9fd
Scott Nicholes <scott93gps@gmail.com>
2018-12-13 12:24:11 -0700

    Starting to create Log In View Controller

---
+++ FamilyShare/LogInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LogInViewController.swift
+//  FamilyShare
+//
+//  Created by Scott Nicholes on 12/13/18.
+//  Copyright © 2018 Scott Nicholes. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
