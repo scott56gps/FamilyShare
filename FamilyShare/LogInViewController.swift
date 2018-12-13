136303ebaa9065e5cbe75bdb1f6e3839ba989bc4
Scott Nicholes <scott93gps@gmail.com>
2018-12-13 13:12:30 -0700

    Created request to log a user in.

--- FamilyShare/LogInViewController.swift
+++ FamilyShare/LogInViewController.swift
@@ -7,24 +7,53 @@
 //
 
 import UIKit
+import Alamofire
 
 class LogInViewController: UIViewController {
-
+    //MARK: Outlets
+    @IBOutlet weak var infoLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: Actions
+    @IBAction func logIn(_ sender: UITextField) {
+        // If there is no userId in UserDefaults, then make a request to get the id
+        let defaults = UserDefaults.standard
+        if (defaults.string(forKey: "User Id") == nil) {
+            // Make parameters
+            var parameters = [String: String]()
+            parameters["username"] = sender.text!
+            
+            // Make a log in request
+            Alamofire.request("https://postgres-query-ancestors.herokuapp.com/login", method: .get, parameters: parameters).responseJSON { response in
+                guard response.result.isSuccess else {
+                    print("GET request for user_id failed: \(String(describing: response.result.error))")
+                    return
+                }
+                
+                guard let value = response.result.value else {
+                    print("Data received was not able to be formed correctly")
+                    return
+                }
+                
+                if let array = value as? [Any] {
+                    for object in array {
+                        let jsonArray = object as? [String: Any]
+                        let userId = jsonArray!["user_id"]! as? String
+                        
+                        // Save the userId in UserDefaults
+                        defaults.set(userId!, forKey: "User Id")
+                    }
+                }
+            }
+        } else {
+            infoLable.text = "User is already logged in"
+        }
     }
-    */
+    
 
 }
