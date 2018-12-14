145a0946407fcce9b5f6e8e7503da78a2d01f165
Scott Nicholes <scott93gps@gmail.com>
2018-12-14 04:16:21 -0700

    FSC-6 Copied code from sharedView to reservedView

--- FamilyShare/ReservedViewController.swift
+++ FamilyShare/ReservedViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import PDFKit
 import Alamofire
 
 class ReservedViewController: UIViewController, UITableViewDataSource {
     var ancestors = [Ancestor]()
+    var templeCard: PDFDocument?
+    
     var defaults = UserDefaults.standard
+    var selectedAncestorsCount = 0
     
     //MARK: Outlets
     @IBOutlet weak var ancestorTableView: UITableView!
@@ -74,6 +78,37 @@ class ReservedViewController: UIViewController, UITableViewDataSource {
         return cell
     }
     
+    //MARK: Actions
+    func showTempleActionSheet(familySearchId: String) {
+        print("I AM IN showTempleActionsSheet!")
+        // Initialize Alert Controller
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertController.Style.actionSheet)
+        
+        // Make actions for Action Sheet
+//        let showCodeAction = UIAlertAction(title: "Show Code", style: UIAlertAction.Style.default, handler: {
+//            (UIAlertAction) -> Void in
+//            print("I am about to showCodeAction")
+//            self.downloadTempleCard(familySearchId: familySearchId)
+//        })
+        
+        let printFORAction = UIAlertAction(title: "Print", style: UIAlertAction.Style.default, handler: {
+            (UIAlertAction) -> Void in
+            self.printFOR()
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler:{(UIAlertAction) -> Void in
+            self.deselectTableViewCells()
+            self.downloadReservedAncestors()
+        })
+        
+        alertController.addAction(showCodeAction)
+        alertController.addAction(printFORAction)
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: true)
+    }
+
+    
     //MARK: Private Functions
     private func downloadReservedAncestors() {
         if let userId = defaults.string(forKey: "User Id") {
@@ -115,4 +150,85 @@ class ReservedViewController: UIViewController, UITableViewDataSource {
             print("User Id nil.  User not signed in")
         }
     }
+    
+    private func downloadTempleCard(familySearchId: String) {
+        if let userId = defaults.string(forKey: "User Id") {
+            // Set the parameters for the GET request
+            let url = "https://postgres-query-ancestors.herokuapp.com/templeCard/" + userId + "/" + familySearchId
+            
+            // Create a place to put the PDF once downloaded
+            let destination: DownloadRequest.DownloadFileDestination = { _, _ in
+                let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+                let fileURL = documentsURL.appendingPathComponent("\(familySearchId).pdf")
+                
+                return (fileURL, [.removePreviousFile, .createIntermediateDirectories])
+            }
+            
+            // Make an Alamofire GET request to get the temple card for this ancestorId
+            Alamofire.download(url, to: destination).response { response in
+                if response.error == nil, let fileURL = response.destinationURL {
+                    print ("PDF Downloaded!")
+                    
+                    if let pdf = PDFDocument(url: fileURL) {
+                        print(pdf.string!)
+                        
+                        self.templeCard = pdf
+                    }
+                } else {
+                    fatalError("PDF was not downloaded correctly")
+                }
+            }
+        } else {
+            fatalError("User Id was not found")
+        }
+    }
+    
+    private func printFOR() {
+        // Get the FOR request
+        
+        
+        // Configure the controller
+        let printController = UIPrintInteractionController.shared
+        printController.printingItem = forRequest
+        
+        // Make Print Info Object
+        let printInfo = UIPrintInfo(dictionary: nil)
+        printInfo.jobName = "FOR Request"
+        
+        printController.printInfo = printInfo
+        
+        printController.present(animated: true, completionHandler: { theHandler, didComplete, errorOptional in
+            self.deselectTableViewCells()
+        })
+    }
+    
+    private func deselectTableViewCells() {
+        if let selectedIndexPaths = self.ancestorTableView.indexPathsForSelectedRows {
+            for indexPath in selectedIndexPaths {
+                self.ancestorTableView.deselectRow(at: indexPath, animated: true)
+            }
+            
+            self.selectedAncestorsCount = 0
+            self.reserveButton.isEnabled = false
+            self.reserveButton.alpha = 0.5
+        }
+    }
+    
+//    func showCodeView() {
+//        UIView.animate(withDuration: 0.5, animations: {
+//            self.sliderConstraint.constant = 0
+//            self.tabBarController?.tabBar.isHidden = true
+//        })
+//    }
+//
+//    func hideCodeView(_ sender: UITapGestureRecognizer) {
+//        UIView.animate(withDuration: 0.5, animations: {
+//            self.sliderConstraint.constant = -400
+//            self.tabBarController?.tabBar.isHidden = false
+//
+//            // Deselect the table view cells
+//            self.deselectTableViewCells()
+//        })
+//    }
+
 }
